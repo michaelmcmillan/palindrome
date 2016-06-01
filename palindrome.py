57028f864267ce4c02d0968fa8b24db4f3441604
Michael McMillan <email@michaelmcmillan.net>
2016-06-01 13:17:30 +0200

    rename index to make the code more readable

--- palindrome.py
+++ palindrome.py
@@ -5,17 +5,17 @@ class Palindrome:
     
     def __init__(self, text):
         self.sentence = Sentence(text)
-        self.current_index = 0
+        self.ordinary_index = 0
         self.opposite_index = self.sentence.length - 1
         self.half_way_index = round(self.sentence.length / 2)
 
     def _increment_and_decrement_indexes(self):
-        self.current_index += 1
+        self.ordinary_index += 1
         self.opposite_index -= 1
 
     def _is_sentence_symmetrical(self):
-        while self.current_index < self.half_way_index:
-            if self.sentence.characters_are_equal(self.current_index, self.opposite_index):
+        while self.ordinary_index < self.half_way_index:
+            if self.sentence.characters_are_equal(self.ordinary_index, self.opposite_index):
                 self._increment_and_decrement_indexes()
             else:
                 return False
